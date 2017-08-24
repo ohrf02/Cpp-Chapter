8ff84725ae68a81352e56dce39ac8e789f0a27ab
ohrf02 <ohrf02@gmail.com>
2017-08-24 10:31:30 +0300

    Update Fraction.cpp

--- Fraction/Fraction.cpp
+++ Fraction/Fraction.cpp
@@ -6,13 +6,16 @@
 #include "Fraction.h"
 #include <cmath>
 
+//Returns the largest common divider of the two given numbers.
 int find_divider(int a, int b);
 
+//The constructor.
 Fraction::Fraction(int nominator, int denominator) {
     this->nominator = nominator;
     this->denominator = denominator;
 }
 
+//The multiplation operator-functions.
 Fraction Fraction::operator*(const Fraction &other) const {
     Fraction f((nominator * other.get_nominator()), (denominator * other.get_denominator()));
     return f;
@@ -23,6 +26,7 @@ Fraction Fraction::operator*(const int num) const {
     return  f;
 }
 
+//The addition operator-functions.
 Fraction Fraction::operator+(const Fraction &other) const {
     int nom, demon;
     nom = (other.denominator * nominator) + (other.nominator * denominator);
@@ -37,6 +41,7 @@ Fraction Fraction::operator+(const int num) const {
     return f;
 }
 
+//The subtraction operator-functions.
 Fraction Fraction::operator-(const Fraction &other) const {
     int nom, demon;
     nom = (other.denominator * nominator) - (other.nominator * denominator);
@@ -51,6 +56,7 @@ Fraction Fraction::operator-(const int num) const {
     return f;
 }
 
+//The division operator-functions.
 Fraction Fraction::operator/(const Fraction &other) const {
     int nom = other.get_nominator();
     int denom = other.get_denominator();
@@ -64,24 +70,29 @@ Fraction Fraction::operator/(const int num) const {
     return Fraction::operator*(f);
 }
 
+//Prints the fraction.
 void Fraction::Print() const{
     std::cout << "( " << nominator;
     std::cout <<" / " << denominator << " )" << std::endl;
 }
 
+//Returns a float of the fraction.
 Fraction::operator float() const {
     return ((float)nominator / (float)denominator);
 }
 
+//Returns a double of the fraction.
 Fraction::operator double() const {
     return ((double)nominator/ (double)denominator);
 }
 
+//Power the fraction by the given number.
 Fraction Fraction::operator^(const int num) const {
     Fraction f((int)pow(nominator, num), (int)pow(denominator, num));
     return f;
 }
 
+//Minimaizes the fraction's elements by the largest common divider.
 void Fraction::minimize() {
     int divider = find_divider(nominator, denominator);
     if (nominator / denominator == 1) {
@@ -93,9 +104,11 @@ void Fraction::minimize() {
 
     }
 }
+
+//Returns the largest common divider of the two given numbers.
 int find_divider(int a, int b){
     int divider = 1;
-    for (int i = 1; i < (a > b ? a : b); ++i) {
+    for (int i = 1; i < (a < b ? a : b); ++i) {
         if(a % i == 0 && b % i == 0)
             divider = i;
     }
